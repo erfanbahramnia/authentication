9d50b00e01ea6c1afce52d2709d4958f6272bd5b
Erfan Bahramnia <erfanbahramnia@gmail.com>
2022-05-15 18:50:50 +0430

    included <br /> for seperate label from input

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,32 +1,40 @@
 import React from "react";
 
+// css style
+import styles from "./SignUp.module.css"
+
 const SignUp = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <h2>SignUp</h2>
-            <form action="#">
+            <form action="#" className={styles.form}>
                 <div>
                     <label>Name</label>
+                    <br />
                     <input type="text" name="name" />
                     <p>error</p>
                 </div>
                 <div>
                     <label>Email</label>
+                    <br />
                     <input type="email" name="email" />
                     <p>error</p>
                 </div>
                 <div>
                     <label>Password</label>
+                    <br />
                     <input type="password" name="pass" />
                     <p>error</p>
                 </div>
                 <div>
                     <label>Confirm password</label>
+                    <br />
                     <input type="password" name="confrimPass" />
                     <p>error</p>
                 </div>
                 <div>
                     <p>I accept terms of privacy policy</p>
+                    <br />
                     <input type="radio" name="privacyButton" />
                     <p>error</p>
                 </div>
